4e252f9b60de468cd5c3fb12276cd0828402e3cf
Matt Stratton <matt.stratton@gmail.com>
2016-09-08 11:07:14 -0500

    Removed unneessary "write-host" piece that was there just for debugging.

--- hugoserver.ps1
+++ hugoserver.ps1
@@ -1,7 +1,6 @@
 # You will need to make sure you have your C drive (or whatever drive you have the devopsdays code on) shared in Docker
 
 $MyPath = (Get-Item -Path ".\" -Verbose).FullName
-Write-Host $MyPath
 
 docker stop hugo-server
 docker rm hugo-server 
