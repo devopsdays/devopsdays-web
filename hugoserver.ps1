92644e1265c43196986ab67c673021240e75f069
Matt Stratton <matt.stratton@gmail.com>
2016-09-08 08:22:13 -0500

    Add powershell script for dockering all the things
    Fixes #1063

---
+++ hugoserver.ps1
@@ -0,0 +1,8 @@
+# You will need to make sure you have your C drive (or whatever drive you have the devopsdays code on) shared in Docker
+
+$MyPath = (Get-Item -Path ".\" -Verbose).FullName
+Write-Host $MyPath
+
+docker stop hugo-server
+docker rm hugo-server 
+docker run -p 1313:1313 -v ${MyPath}:/site --name hugo-server devopsdays/docker-hugo-server:v0.16
