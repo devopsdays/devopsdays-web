02634e5d4848076547b4642b1fc19b62a12cf832
Matt Stratton <matt.stratton@gmail.com>
2018-09-03 12:56:14 -0700

    Remove deprecated gulp tasks

--- gulp/tasks/development/watch.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp'),
-  watch = require('gulp-watch');
-
-gulp.task('watch', function() {
-  gulp.watch('static/scss/*.scss', ['sass']);
-  gulp.watch('static/js/devopsdays.js', ['js-concat']);
-})
