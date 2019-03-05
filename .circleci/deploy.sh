112b912bbbc6b639608a2d04911e4aaa0d66ffe0
Matt Stratton <matt.stratton@gmail.com>
2019-03-04 17:53:33 -0800

    Add GH Pages step to CircleCI

---
+++ .circleci/deploy.sh
@@ -0,0 +1,14 @@
+hugo -v # command to build your site
+cd public # cd into folder containing your built site
+
+git init
+git config user.name "Matt Stratton"
+git config user.email "matt.stratton@gmail.com"
+
+echo "www.devopsdays.com" > CNAME # domain your site will live at
+git add .
+git commit -m "Deploy from CircleCI" # this will always be the only commit in your gh-pages branch
+
+# use --quiet to avoid printing token in build logs
+git push --force --quiet "https://${GH_TOKEN}@github.com/devopsdays/devopsdays-web.git" master:gh-pages # path to your repo on GitHub, using token for authentication
+
