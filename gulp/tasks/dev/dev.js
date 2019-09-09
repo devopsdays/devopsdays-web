d960a71730420ca782ad1d2d9adccdb252aa087f
Matt Stratton <matt.stratton@gmail.com>
2019-09-08 22:11:53 -0500

    Add gulp task for minifying CSS
    Signed-off-by: Matt Stratton <matt.stratton@gmail.com>

--- gulp/tasks/dev/dev.js
+++ gulp/tasks/dev/dev.js
@@ -8,4 +8,4 @@ var hub = new HubRegistry(['tasks/dev/*.js']);
 
 // tell gulp to use the tasks just loaded
 gulp.registry(hub);
-gulp.task('dev', gulp.series('js-concat', 'sass'))
+gulp.task('dev', gulp.series('js-concat', 'sass', 'minify-css'))
