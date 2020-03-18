513865abe082c75431b734bab230d0c0a17d8d92
Garrett Honeycutt <code@garretthoneycutt.com>
2020-03-17 21:02:10 -0400

    Standardize on Hugo usage
    This standardized on using the cibuilds/hugo docker image instead of the
    standard jojomi/hugo as well as using version 0.67.1 of hugo everywhere.
    
    See https://github.com/devopsdays/devopsdays-web/pull/9638 for more history.

--- hugoserver.sh
+++ hugoserver.sh
@@ -11,4 +11,4 @@
 
 docker stop hugo-server
 docker rm   hugo-server
-docker run -tip 1313:1313 -v $(pwd):/src:cached -e HUGO_THEME=devopsdays-theme -e HUGO_WATCH=1 -e HUGO_BASEURL="http://localhost:1313" --name hugo-server jojomi/hugo:0.67.0
+docker run -tip 1313:1313 -v $(pwd):/home/circleci/project:cached -e HUGO_THEME=devopsdays-theme -e HUGO_BASEURL="http://localhost:1313" --name hugo-server --entrypoint "" cibuilds/hugo:0.67.0 hugo server --watch --bind ""
