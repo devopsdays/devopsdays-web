828a17d680562e2179215a39bc21e5d56d7a82e1
Matt Kuritz <mkuritz@thefarmersdog.com>
2023-04-20 14:36:11 -0400

    more cleanup

--- .github/scripts/comment-pr-large-file.js
+++ .github/scripts/comment-pr-large-file.js
@@ -1,18 +1,17 @@
 module.exports = async ({github, context, core}) => {
-  const pr = context.payload.pull_request;
-  const repo = context.repo;
-  const issueNumber = pr.number;
-  const { data: comments } = await github.issues.listComments({
-    ...repo,
-    issue_number: issueNumber,
+  const { data: comments } = await github.rest.issues.listComments({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: context.payload.pull_request.number,
   });
-  const botComment = comments.find((comment) =>
+  const largeFileComment = comments.find((comment) =>
     comment.body.startsWith(":warning: Warning: This PR contains a PDF or large file")
   );
-  if (!botComment) {
-    await github.issues.createComment({
-      ...repo,
-      issue_number: issueNumber,
+  if (!largeFileComment) {
+    await github.rest.issues.createComment({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+      issue_number: context.payload.pull_request.number,
       body: ":warning: Warning: This PR contains a PDF or large file. In most cases, you want to use the [asset repository](https://github.com/devopsdays/devopsdays-assets/)",
     });
   }
