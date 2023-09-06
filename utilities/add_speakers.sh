3d92859b597aca91c1caeedd97636cd781a0e0d9
rex roof <github@rexroof.com>
2023-09-06 18:13:23 -0400

    created new common function "replace_string" to be used in place of sedcmd: replaces a string in a file, escaping any potentially troublesome characters beforehand.

--- utilities/add_speakers.sh
+++ utilities/add_speakers.sh
@@ -33,12 +33,11 @@ mkdir -p ../static/events/$event_slug/speakers
 # Create empty speakers page file (will be auto-filled for display)
 speakerspage="../content/events/$event_slug/speakers.md"
 cp examples/templates/speakers.md $speakerspage
-sedcmd "s/CITY/$city/" $speakerspage
-sedcmd "s/YYYY/$year/" $speakerspage
+string_replace "CITY" "${city}" "${speakerspage}"
+string_replace "YYYY" "${year}" "${speakerspage}"
 
 # uncomment link to speakers page
-sedcmd "s/#  - name: speakers/  - name: speakers/" ../data/events/$event_slug.yml
-
+string_replace "#  - name: speakers" "  - name: speakers" "../data/events/${event_slug}.yml"
 
 # Prompt for inputting speakers
 while [ 1 ]
@@ -55,15 +54,15 @@ speaker_slug=$(echo $speakername | tr -dc '[:alpha:][:blank:]' | tr '[:upper:]' 
 speakerfile="../content/events/$event_slug/speakers/$speaker_slug.md"
 cp examples/templates/speakers-speaker-full-name.md $speakerfile
 
-sedcmd "s/SPEAKERNAME/$speakername/" $speakerfile
-sedcmd "s/SPEAKERSLUG/$speaker_slug/" $speakerfile
+string_replace "SPEAKERNAME" "${speakername}" "${speakerfile}"
+string_replace "SPEAKERSLUG" "${speaker_slug}" "${speakerfile}"
 
 # twitter handle
 read -p "Enter speaker twitter handle (return for none): " twitter
 [ -z "${twitter}" ] && twitter=''
 # remove @ if they added it
 twitter=$(echo $twitter | sed 's/@//')
-sedcmd "s/SPEAKERTWITTER/$twitter/" $speakerfile
+string_replace "SPEAKERTWITTER" "${twitter}" "${speakerfile}"
 
 #linkedin profile
 read -p "Enter speaker linkedin profile (return for none): " linkedin
@@ -74,7 +73,7 @@ sedcmd "s,SPEAKERLINKEDIN,$linkedin,g" $speakerfile
 # bio
 read -p "Enter speaker bio (return for none): " bio
 [ -z "${bio}" ] && bio=''
-sedcmd "s_SPEAKERBIO_${bio}_" $speakerfile
+string_replace "SPEAKERBIO" "${bio}" "${speakerfile}"
 
 ####################
 # Populate talk file
@@ -82,32 +81,32 @@ sedcmd "s_SPEAKERBIO_${bio}_" $speakerfile
 talkfile="../content/events/$event_slug/program/$speaker_slug.md"
 cp examples/templates/program-speaker-full-name.md $talkfile
 
-sedcmd "s/SPEAKERSLUG/$speaker_slug/" $talkfile
+string_replace "SPEAKERSLUG" "${speaker_slug}" "${talkfile}"
 
 # talk title
 read -p "Enter speaker talk title (return for none): " title
 [ -z "${title}" ] && title=''
-sedcmd "s_TALKTITLE_${title}_" $talkfile
+string_replace "TALKTITLE" "${title}" "${talkfile}"
 
 # talk description
 read -p "Enter speaker talk description (return for none): " abstract
 [ -z "${abstract}" ] && abstract=''
-sedcmd "s_ABSTRACT_${abstract}_" $talkfile
+string_replace "ABSTRACT" "${abstract}" "${talkfile}"
 
 #######################
 # Set speaker image
 #######################
 #
 
-# if speaker image is already in expected location as .png
+# if speaker image is already in expected location
 if [[ -f "../static/events/$event_slug/speakers/$speaker_slug.png" ]] ; then
-  sedcmd "s/image = \"\"/image = \"$speaker_slug.png\"/" $speakerfile
-  echo "found $speaker_slug.png, using"
+  string_replace "image = \"\"" "image = \"${speaker_slug}.png\"" "${speakerfile}"
+  echo "found ${speaker_slug}.png, using"
 else
   read -p "Enter path to speaker image PNG: " speakerimage
   if [[ -n "${speakerimage}" ]] && [[ -f "${speakerimage}" ]] ; then
     cp -v "${speakerimage}" "../static/events/${event_slug}/speakers/${speaker_slug}.png"
-    sedcmd "s/image = \"\"/image = \"$speaker_slug.png\"/" $speakerfile
+    string_replace "image = \"\"" "image = \"${speaker_slug}.png\"" "${speakerfile}"
   else
     echo "no image used.   If desired:
       1. Copy PNG into ../static/events/${event_slug}/speakers/${speaker_slug}.png
