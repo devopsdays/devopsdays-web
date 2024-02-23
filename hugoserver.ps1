b67b4f9f2bfb841ab89bd0f48a15f4441394b10b
Matty Stratton <matty.stratton@aiven.io>
2024-02-23 12:32:59 -0600

    Update hugo to 0.123.3
    NOTE: the docker scripts are on 0.123.2 because the cibuilds has not added a tag for 0.123.3 yet
    Signed-off-by: Matty Stratton <matty.stratton@aiven.io>

--- hugoserver.ps1
+++ hugoserver.ps1
@@ -5,4 +5,4 @@ $MyPath = $PSScriptRoot
 
 docker stop hugo-server
 docker rm hugo-server
-docker run -tip 1313:1313 -v $(pwd):/home/circleci/project:cached -e HUGO_THEME=devopsdays-theme -e HUGO_BASEURL="http://localhost:1313" --name hugo-server --entrypoint "" cibuilds/hugo:0.121.2 hugo server --watch --bind ""
+docker run -tip 1313:1313 -v $(pwd):/home/circleci/project:cached -e HUGO_THEME=devopsdays-theme -e HUGO_BASEURL="http://localhost:1313" --name hugo-server --entrypoint "" cibuilds/hugo:0.123.2 hugo server --watch --bind ""
