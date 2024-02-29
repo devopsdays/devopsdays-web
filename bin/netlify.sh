717529f5f87fb181833a5f7af09522e1e2b8872d
Matty Stratton <matty.stratton@aiven.io>
2024-02-29 16:39:04 -0600

    try dart sass plugin
    Signed-off-by: Matty Stratton <matty.stratton@aiven.io>

--- bin/netlify.sh
+++ bin/netlify.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-echo "Install Dart Sass..."
-
-# This is in Netlify's PATH.
-# npm install -g sass
-
-echo "Building..."
-
 hugo version
 hugo --theme=devopsdays-theme --buildDrafts=false --baseURL="$DEPLOY_PRIME_URL" --gc --minify
 cp bin/staging-robots.txt public/robots.txt
