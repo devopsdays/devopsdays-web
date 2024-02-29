6f0778d269942272d19b0e0f0b599bc6f9a261ea
Matty Stratton <matt.stratton@gmail.com>
2024-02-29 21:55:48 +0000

    Hardcode path

--- bin/netlify.sh
+++ bin/netlify.sh
@@ -8,9 +8,10 @@ DARTSASS_VERSION=1.71.1,
 
 mkdir -p $BIN_DIR
 
-curl -LJO https://github.com/sass/dart-sass/releases/download/${DARTSASS_VERSION}/dart-sass-${DARTSASS_VERSION}-linux-x64.tar.gz
+#curl -LJO https://github.com/sass/dart-sass/releases/download/${DARTSASS_VERSION}/dart-sass-${DARTSASS_VERSION}-linux-x64.tar.gz
+curl -LJO https://github.com/sass/dart-sass/releases/download/1.71.1/dart-sass-1.71.1-linux-x64.tar.gz
 
-tar -xvf dart-sass-${DARTSASS_VERSION}-linux-x64.tar.gz;
+tar -xvf dart-sass-1.71.1-linux-x64.tar.gz;
 
 mv dart-sass/sass $BIN_DIR
 
