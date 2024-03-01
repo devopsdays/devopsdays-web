fdde8f766a9c848f1d40564130c2a4d042fb2b7c
Matty Stratton <matty.stratton@aiven.io>
2024-02-29 18:05:55 -0600

    Clean up dart sass install
    Signed-off-by: Matty Stratton <matty.stratton@aiven.io>

--- bin/netlify.sh
+++ bin/netlify.sh
@@ -8,17 +8,12 @@ DARTSASS_VERSION=1.71.1,
 
 mkdir -p $BIN_DIR
 
-#curl -LJO https://github.com/sass/dart-sass/releases/download/${DARTSASS_VERSION}/dart-sass-${DARTSASS_VERSION}-linux-x64.tar.gz
-curl -LJO https://github.com/sass/dart-sass/releases/download/1.71.1/dart-sass-1.71.1-linux-x64.tar.gz
+curl -LJO https://github.com/sass/dart-sass/releases/download/${DARTSASS_VERSION}/dart-sass-${DARTSASS_VERSION}-linux-x64.tar.gz
+#curl -LJO https://github.com/sass/dart-sass/releases/download/1.71.1/dart-sass-1.71.1-linux-x64.tar.gz
 
-tar -xvf dart-sass-1.71.1-linux-x64.tar.gz
-rm dart-sass-1.71.1-linux-x64.tar.gz
+tar -xvf dart-sass-${DARTSASS_VERSION}-linux-x64.tar.gz
+rm dart-sass-${DARTSASS_VERSION}-linux-x64.tar.gz
 export PATH=/opt/build/repo/dart-sass:$PATH
-echo "ls after untar"
-echo "PATH is $PATH"
-echo "PWD is:"
-pwd
-
 
 sass --embedded --version
 
