a6c65b269a490f3d92d4ea389be8045afcc9cffb
Toshaan Bharvani <toshaan@vantosh.com>
2025-12-07 22:42:27 +0100

    get the name of the old file correct
    Signed-off-by: Toshaan Bharvani <toshaan@vantosh.com>

--- bin/netlify-production.sh
+++ bin/netlify-production.sh
@@ -3,4 +3,4 @@
 hugo version
 hugo --environment=production --theme=devopsdays-theme --minify --templateMetrics --logLevel info --buildDrafts=false --baseURL="$URL/"
 CSSFINGERPRINTED=$(ls public/css/site.min.*.css)
-cp -f $CSSFINGERPRINTED public/css/site.min.css
+cp -f $CSSFINGERPRINTED public/css/site.css
