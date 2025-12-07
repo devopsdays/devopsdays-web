50b4310341edd19de060671a6a06e7894276ea11
Toshaan Bharvani <toshaan@vantosh.com>
2025-12-07 14:31:51 +0100

    update netlify deploy
    * improve preview feedback to provide more output
    * add `site.min.css` symbolic link for archived sites
    
    Signed-off-by: Toshaan Bharvani <toshaan@vantosh.com>

--- bin/netlify-production.sh
+++ bin/netlify-production.sh
@@ -1,4 +1,7 @@
-# npm rebuild
+#!/bin/bash
+
+rm -rf public/css/site.min.css
 hugo version
-hugo --theme=devopsdays-theme --buildDrafts=false --baseURL="$URL/"
-# gulp
+hugo --environment=production --theme=devopsdays-theme --minify --templateMetrics --logLevel info --buildDrafts=false --baseURL="$URL/"
+CSSFINGERPRINTED=$(ls public/css/site.min.*.css)
+ln -s $(basename $CSSFINGERPRINTED) public/css/site.min.css
