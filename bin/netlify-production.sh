957478ff612d589eccaa629cdfd9bc8b5692df6d
Toshaan Bharvani <toshaan@vantosh.com>
2025-12-07 22:09:01 +0100

    change to use copy file
    * netlify does not support symbolic links
    * copy the site.min.{fingerprint}.css to site.min.css
    
    Signed-off-by: Toshaan Bharvani <toshaan@vantosh.com>

--- bin/netlify-production.sh
+++ bin/netlify-production.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-rm -rf public/css/site.min.css
 hugo version
 hugo --environment=production --theme=devopsdays-theme --minify --templateMetrics --logLevel info --buildDrafts=false --baseURL="$URL/"
 CSSFINGERPRINTED=$(ls public/css/site.min.*.css)
-ln -s $(basename $CSSFINGERPRINTED) public/css/site.min.css
+cp -f $CSSFINGERPRINTED public/css/site.min.css
--- bin/netlify.sh
+++ bin/netlify.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-rm -rf public/css/site.min.css
 hugo version
 hugo --environment=development --theme=devopsdays-theme --buildDrafts=false --templateMetrics --templateMetricsHints --printMemoryUsage --printPathWarnings --printUnusedTemplates --printI18nWarnings --logLevel debug --baseURL="$DEPLOY_PRIME_URL"
 cp bin/staging-robots.txt public/robots.txt
 CSSFINGERPRINTED=$(ls public/css/site.min.*.css)
-ln -s $(basename $CSSFINGERPRINTED) public/css/site.min.css
+cp -fv $CSSFINGERPRINTED public/css/site.min.css
